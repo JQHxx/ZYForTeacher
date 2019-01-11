58b137ef0c37348886d752409207a26a52ebf72c
vision <vision@visiondeiMac.local>
2019-01-11 16:30:19 +0800

    20190111

---
+++ ZYForTeacher/Classes/Main/Tools/ZYCustomPageControl.h
@@ -0,0 +1,20 @@
+//
+//  ZYCustomPageControl.h
+//  ZuoYe
+//
+//  Created by vision on 2018/9/15.
+//  Copyright © 2018年 vision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYCustomPageControl : UIPageControl
+
+@property (nonatomic, strong) UIImage *currentImage;
+@property (nonatomic, strong) UIImage *inactiveImage;
+
+@property (nonatomic, assign) CGSize currentImageSize;
+@property (nonatomic, assign) CGSize inactiveImageSize;
+
+
+@end
---
+++ ZYForTeacher/Classes/Main/Tools/ZYCustomPageControl.m
@@ -0,0 +1,61 @@
+//
+//  ZYCustomPageControl.m
+//  ZuoYe
+//
+//  Created by vision on 2018/9/15.
+//  Copyright © 2018年 vision. All rights reserved.
+//
+
+#import "ZYCustomPageControl.h"
+
+
+@implementation ZYCustomPageControl
+
+-(instancetype)init{
+    self = [super init];
+    if (self) {
+        self.userInteractionEnabled = NO;
+    }
+    return self;
+}
+
+- (void)setCurrentPage:(NSInteger)currentPage{
+    [super setCurrentPage:currentPage];
+    
+    [self updateDots];
+}
+
+#pragma mark 更新点
+- (void)updateDots{
+    for (int i = 0; i < [self.subviews count]; i++) {
+        UIImageView *dot = [self imageViewForSubview:[self.subviews objectAtIndex:i] currPage:i];
+        if (i == self.currentPage){
+            dot.image = self.currentImage;
+            dot.size = self.currentImageSize;
+        }else{
+            dot.image = self.inactiveImage;
+            dot.size = self.inactiveImageSize;
+        }
+    }
+}
+- (UIImageView *)imageViewForSubview:(UIView *)view currPage:(int)currPage{
+    UIImageView *dot = nil;
+    if ([view isKindOfClass:[UIView class]]) {
+        for (UIView *subview in view.subviews) {
+            if ([subview isKindOfClass:[UIImageView class]]) {
+                dot = (UIImageView *)subview;
+                break;
+            }
+        }
+        
+        if (dot == nil) {
+            dot = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, view.frame.size.width, view.frame.size.height)];
+            [view addSubview:dot];
+        }
+    }else {
+        dot = (UIImageView *)view;
+    }
+    return dot;
+}
+
+@end
